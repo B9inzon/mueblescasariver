44d2f7b3a2f6ac0ab453f358b0baafcc695fa51a
Brandon Pinzon <brandonpinzont@gmail.com>
2025-04-14 15:33:50 -0500

    Tailwind

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <div className="flex flex-col h-[70%] lg:h-[60vh]  py-10 items-center justify-around text-[#3C3A36] leading-10  ">
-        <h1 className='flex font-bold text-xl  h-50 items-center justify-center md:text-2xl w-[80%] border-t-2 border-b-2 border-[#3c3836] '>Siguenos en @mueblescasariver</h1>
+        <h1 className='flex font-bold text-xl  h-50 items-center justify-center mb-8 md:text-2xl w-[80%] border-b-2 border-[#3c3836] '>Siguenos en @mueblescasariver</h1>
         <div className='flex flex-col lg:flex-row w-full text-base lg:text-lg  leading-10 text-center lg:text-start items-center lg:items-start lg:justify-evenly '>
             <ul className='mb-10'>
                 <h3 className=' font-semibold  text-xl lg:text-2xl'>Sobre nosotros</h3>
