ce1ee9c04cd0efe2bb58c1cf912d81a0558507c5
Brandon Pinzon <brandonpinzont@gmail.com>
2025-05-05 20:32:29 -0500

    Added about page and its content

---
+++ front/src/app/about/page.jsx
@@ -0,0 +1,48 @@
+import { VisionIcon } from "@/components/icons/VisionIcon";
+import { Logo } from "@/components/Logo";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex flex-col w-full py- justify-center  items-center text-[#3c3a36] ">
+      <div className=" relative flex flex-col items-center justify-center w-full h-screen lg:w-full p-4 mb-8   lg:p-12  bg-[url('/assets/Living-3000.jpg')] grayscale-75 bg-cover bg-bottom">
+        <Logo  />
+        <div className=" absolute inset-0 -z-1   bg-black opacity-60  "></div>
+      </div>
+      <h1 className="font-semibold text-center w-[40vw] text-4xl mb-10 ">
+        Sobre Muebles Casa River
+      </h1>
+      <p className="w-[40vw] text-justify font-medium text-base">
+        <span className="font-bold">Muebles Casa River</span> es un catálogo en
+        línea donde puedes ver nuestros productos, solicitar una cotización o
+        simplemente contactarnos para recibir atención personalizada e
+        información sobre el producto de tu interés. Somos una iniciativa que
+        busca crear un canal de comunicación efectivo entre el cliente y los
+        diferentes almacenes y fabricantes asociados, para acercarte información
+        de primera mano y brindarte la posibilidad de cotizar o comprar desde el
+        lugar en el que te encuentres.
+      </p>
+      <br />
+      <p className="w-[40vw] text-justify font-medium text-base mb-10">
+        Con más de 25 años de experiencia fabricando, diseñando y distribuyendo
+        muebles de excelente calidad y diseño a precios justos, en <span className="font-bold">Muebles Casa River</span> tenemos como
+        prioridad la satisfacción de nuestros clientes. Por ello, nos
+        comprometemos a brindar una atención personalizada que los haga sentir
+        como si estuvieran en uno de nuestros almacenes físicos, y que les
+        permita elegir con total confianza el mueble perfecto para su hogar. A
+        través de este canal, garantizamos un servicio cercano y personalizado,
+        sin importar dónde se encuentren, porque sabemos que tu próximo mueble
+        favorito lo encuentras con nosotros.
+      </p>
+      <div className="flex justify-around p-2 items-center border-2 rounded-lg  w-[40vw]">
+        <VisionIcon />
+        <p className="text-justify font-semibold w-[60%] ">
+          Nuestro objetivo es ofrecer a nuestros clientes, en un solo lugar y de
+          manera cómoda e innovadora, un amplio catálogo de productos de
+          diferentes almacenes y fabricantes asociados, facilitando así el acceso a opciones de
+          calidad que se adapten a sus gustos y necesidades.
+        </p>
+      </div>
+    </div>
+  );
+}
--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -5,11 +5,14 @@ export const Footer = () => {
   return (
     <div className="flex flex-col h-auto lg:h-[70vh]  py-10 items-center justify-around text-[#3C3A36] leading-10  ">
       <div className="flex flex-col mb-12 h-[30%] justify-center items-center w-full ">
+        <div className="h-2 border-b-2 w-[70vw] mb-10"></div>
         <h1 className="flex font-bold text-md   h-auto items-center justify-center mb-8 md:text-2xl w-[80%]  ">
           Siguenos en @mueblescasariver
         </h1>
         <InstagramIcon />
+
       </div>
+        <div className="h-2 border-b-2 w-[70vw] mb-10"></div>
       <div className="flex flex-col lg:flex-row w-full text-base lg:text-lg  leading-10 text-center lg:text-start items-center lg:items-start lg:justify-evenly ">
         <ul className="mb-10">
           <li className=" font-semibold  text-xl lg:text-2xl">
