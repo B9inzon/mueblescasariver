ba79a472b65a1a29107d0c4168e698f2970136c4
Brandon Pinzon <brandonpinzont@gmail.com>
2025-05-23 17:52:40 -0500

    Updated footer component

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { InstagramIcon } from "./icons/InstagramIcon";
 import Link from "next/link";
+import { WhatsappIcon } from "./icons/WhatsappIcon";
+import { PhoneIcon } from "./icons/PhoneIcon";
+import { MailIcon } from "./icons/MailIcon";
 
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -25,38 +28,66 @@ export const Footer = () => {
 
         <div className=" grid grid-cols-1 lg:grid-cols-3 gap-8 text-center lg:text-left  ">
           <nav aria-label="Sobre nosotros">
-            <h3 className=" font-semibold text-base mb-4 ">Sobre nosotros</h3>
-            <ul className=" space-y-2 ">
+            <h3 className=" font-semibold text-base mb-4 ">
+              Muebles Casa River
+            </h3>
+            <ul className=" space-y-4 ">
               <li className=" hover:underline focus:underline focus:outline-none ">
                 <Link href="/about">Quienes somos</Link>
               </li>
               <li className=" hover:underline focus:underline focus:outline-none ">
-                <Link href="/contact">Contáctanos</Link>
+                <Link href="/contact">Contácto</Link>
               </li>
               <li className=" hover:underline focus:underline focus:outline-none ">
                 <Link href="/contact">Vende en Muebles Casa River</Link>
               </li>
+              <li className=" hover:underline focus:underline focus:outline-none ">
+                <Link href="/products">Nuestros productos</Link>
+              </li>
             </ul>
           </nav>
           <nav aria-label="Catálogo">
-            <h3 className=" font-semibold text-base mb-4 ">
-              Nuestros Productos
-            </h3>
-            <ul className=" space-y-2 ">
+            <h3 className=" font-semibold text-base mb-4 ">Contáctanos</h3>
+            <ul className=" space-y-4 ">
               <li className=" hover:underline focus:underline focus:outline-none ">
-                <Link href="/products">Catálogo de productos</Link>
+                <Link
+                  href="https://wa.link/0nego0"
+                  target="_blank"
+                  rel=" noopener noreferrer "
+                  aria-label=" Escríbenos al WhatsApp "
+                  className="flex items-center gap-2"
+                >
+                  <WhatsappIcon  /> +57 301 315 1460
+                </Link>
+              </li>
+              <li className=" hover:underline focus:underline focus:outline-none ">
+                <span
+                  aria-label=" Llámanos a este número "
+                  className="flex items-center gap-2"
+                >
+                  <PhoneIcon  /> +57 301 315 1460
+                </span>
+              </li>
+              <li className=" hover:underline focus:underline focus:outline-none ">
+                <span
+                  aria-label="Escríbenos a este correo"
+                  className="flex items-center gap-2"
+                >
+                  <MailIcon  /> mueblescasariver@gmail.com
+                </span>
               </li>
             </ul>
           </nav>
           <nav aria-label="Sobre nosotros">
             <h3 className=" font-semibold text-base mb-4 ">Información</h3>
-            <ul className=" space-y-2 ">
+            <ul className=" space-y-4 ">
               <li className=" hover:underline focus:underline focus:outline-none ">
                 <Link href="/terms">Términos y Condiciones</Link>
               </li>
               <li className=" hover:underline focus:underline focus:outline-none ">
-                <Link href="https://sedeelectronica.sic.gov.co/"
-                target="_blank"
+                <Link
+                  href="https://sedeelectronica.sic.gov.co/"
+                  target="_blank"
                 >
                   Superintendencia de Industria y Comercio
                 </Link>
