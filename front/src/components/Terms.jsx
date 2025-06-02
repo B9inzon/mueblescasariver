62c3ae5b42380764970c34d6719157c7486311c4
Brandon Pinzon <brandonpinzont@gmail.com>
2025-06-02 17:23:13 -0500

    Tailwind

--- front/src/components/Terms.jsx
+++ front/src/components/Terms.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Terms() {
   return (
     <section className="flex flex-col pt-38  min-h-screen text-[#3c3a36]">
-      <h1 className="text-center w-full text-[5vw] font-medium md:text-[6vw] lg:text-[2.2vw] mb-8">
+      <h1 className="text-center w-full text-[1.2rem] md:text-[1.5rem] uppercase font-medium  mb-8">
         Términos y condiciones
       </h1>
-      <p className="w-1/2 text-justify mx-auto mb-12">
+      <p className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-12">
         Gracias por elegir nuestro taller artesanal de fabricación de muebles.
         Estos términos y condiciones regulan la relación entre el cliente y el
         taller en la compra de muebles personalizados. Al visitar, revisar el
@@ -14,10 +14,10 @@ export default function Terms() {
         mediante cualquier medio, el cliente acepta expresamente estas
         condiciones.
       </p>
-      <h4 className="text-center w-full text-[5vw] font-medium md:text-[6vw] lg:text-lg mb-4">
+      <h4 className="text-center w-full text-[1rem] md:text-[1.2rem] font-medium   mb-4">
         RESERVA DE MERCANCIA
       </h4>
-      <p className="w-1/2 text-justify mx-auto mb-20">
+      <p className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-20">
         Cuando el cliente entregue una suma de dinero como anticipo para separar
         mercancía, como parte del precio, como manifestación de acuerdo entre
         las partes, o por cualquier motivo similar, se entenderá que, en caso de
@@ -25,84 +25,84 @@ export default function Terms() {
         compraventa, perderá dicho anticipo a título de cláusula penal, conforme
         a lo previsto en el artículo 867 del Código de Comercio.
       </p>
-      <h4 className="text-center w-full text-[5vw] font-medium md:text-[6vw] lg:text-lg mb-4">
+      <h4 className="text-center w-full text-[1rem] md:text-[1.2rem] font-medium   mb-4">
         1. NATURALEZA DEL SERVICIO
       </h4>
-      <span className="w-1/2 text-justify mx-auto mb-4">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4">
         1.1. Somos un taller que se dedica a la fabricación de muebles
         personalizados, tales como sofás, sillas, poltronas entre otros.
       </span>
-      <span className="w-1/2 text-justify mx-auto mb-4">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4">
         1.2. Todos los muebles se fabrican bajo pedido, según las
         especificaciones acordadas con el cliente, incluyendo tipo de material,
         color, forma y tamaño.
       </span>
-      <span className="w-1/2 text-justify mx-auto mb-4">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4">
         1.3. El cliente entiende y acepta que puede haber ligeras variaciones
         entre el producto final y las referencias visuales (fotos o muestras)
         proporcionadas.
       </span>
-      <span className="w-1/2 text-justify mx-auto mb-4">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4">
         1.4. La página web funciona únicamente como un catálogo informativo. Los
         pedidos no se realizan en línea, sino a través de contacto directo con
         un vendedor vía WhatsApp u otro canal oficial indicado.
       </span>
-      <span className="w-1/2 text-justify mx-auto mb-8">
-        1.5. La empresa no ofrece servicios de transporte o envío. El retiro del
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-8">
+        1. La empresa no ofrece servicios de transporte o envío. El retiro del
         producto es responsabilidad exclusiva del cliente, ya sea de forma
         personal o contratando un servicio logístico externo.
       </span>
-      <h4 className="text-center w-full text-[5vw] font-medium md:text-[6vw] lg:text-lg mb-4">
+      <h4 className="text-center w-full text-[1rem] md:text-[1.2rem] font-medium   mb-4">
         2. PEDIDOS Y PAGOS
       </h4>
-      <span className="w-1/2 text-justify mx-auto mb-4">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4">
         2.1. Para iniciar la fabricación, el cliente deberá abonar un anticipo
         del 30% del valor total del mueble. Dicho anticipo es obligatorio y no
         reembolsable.
       </span>
-      <span className="w-1/2 text-justify mx-auto mb-4">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4">
         2.2. El saldo restante deberá ser abonado antes de la entrega o retiro
         del producto. No se harán entregas si no se ha completado el pago total.
       </span>
-      <span className="w-1/2 text-justify mx-auto mb-4">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4">
         2.3. Cualquier modificación posterior al acuerdo inicial (tipo de tela,
         estructura, tamaño, color u otros materiales) estará sujeta a un nuevo
         presupuesto y puede afectar el tiempo de entrega previamente estipulado.
       </span>
-      <span className="w-1/2 text-justify mx-auto mb-4">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4">
         2.4. Los precios publicados en el catálogo web son referenciales. El
         precio final se confirma directamente con el vendedor tras definir los
         detalles personalizados del pedido.
       </span>
-      <span className="w-1/2 text-justify mx-auto mb-8">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-8">
         2.5. Los pagos pueden realizarse por los medios informados oficialmente
         por el vendedor. El cliente es responsable de verificar la cuenta antes
         de transferir fondos.
       </span>
 
-      <h4 className="text-center w-full text-[5vw] font-medium md:text-[6vw] lg:text-lg mb-4">
+      <h4 className="text-center w-full text-[1rem] md:text-[1.2rem] font-medium   mb-4">
         3. TIEMPOS DE ENTREGA
       </h4>
-      <span className="w-1/2 text-justify mx-auto mb-4">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4">
         3.1. El plazo de fabricación se informan al momento de confirmar el
         pedido. Dicho plazo es estimado y puede variar dependiendo de la
         complejidad del mueble, disponibilidad de materiales, y carga de trabajo
         del taller.
       </span>
-      <span className="w-1/2 text-justify mx-auto mb-8">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-8">
         3.2. En caso de fuerza mayor (retrasos en insumos, condiciones
         climáticas, problemas logísticos, etc.), el cliente será notificado a la
         brevedad, y se acordará una nueva fecha razonable para la entrega.
       </span>
 
-      <h4 className="text-center w-full text-[5vw] font-medium md:text-[6vw] lg:text-lg mb-4">
+      <h4 className="text-center w-full text-[1rem] md:text-[1.2rem] font-medium   mb-4">
         4. GARANTÍA Y COBERTURA
       </h4>
-      <span className="w-1/2 text-justify mx-auto mb-2">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-2">
         4.1. Todos los muebles tienen una garantía de 6 meses desde la fecha de
         retiro, la cual cubre:
       </span>
-      <ul className="w-1/2 text-justify mx-auto mb-4 ">
+      <ul className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4 ">
         <li className="list-inside list-disc">Costuras defectuosas.</li>
         <li className="list-inside list-disc">
           Daños en la estructura (evaluados según el tipo y origen del daño).
@@ -111,10 +111,10 @@ export default function Terms() {
           Fallas en bisagras o mecanismos funcionales.
         </li>
       </ul>
-      <span className="w-1/2 text-justify mx-auto mb-2">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-2">
         4.2. La garantía NO cubre:
       </span>
-      <ul className="w-1/2 text-justify mx-auto mb-4 ">
+      <ul className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4 ">
         <li className="list-inside list-disc">
           Daños causados por el transporte (golpes, rayones, fracturas, etc.).
         </li>
@@ -131,46 +131,46 @@ export default function Terms() {
           naturales.
         </li>
       </ul>
-      <span className="w-1/2 text-justify mx-auto mb-4">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4">
         4.3. Para hacer uso de la garantía, el cliente debe presentar el
         comprobante de pago y enviar evidencia fotográfica del problema. El
         mueble podrá ser inspeccionado en el taller para evaluar si el daño
         entra dentro de la cobertura.
       </span>
-      <span className="w-1/2 text-justify mx-auto mb-8">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-8">
         4.4. La garantía no incluye gastos de traslado del mueble al taller para
         su revisión o reparación. Estos deben ser asumidos por el cliente.
       </span>
 
-      <h4 className="text-center w-full text-[5vw] font-medium md:text-[6vw] lg:text-lg mb-4">
+      <h4 className="text-center w-full text-[1rem] md:text-[1.2rem] font-medium   mb-4">
         5. REVISIÓN Y ENTREGA DEL PRODUCTO
       </h4>
-      <span className="w-1/2 text-justify mx-auto mb-4">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4">
         5.1. El cliente debe revisar el mueble en el taller antes de retirarlo.
         Esto incluye verificar costuras, estructuras, color, manchas, rayones y
         funcionamiento de bisagras o mecanismos.
       </span>
-      <span className="w-1/2 text-justify mx-auto mb-4">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4">
         5.2. Una vez el mueble es retirado o entregado a un tercero designado
         por el cliente, no se aceptarán reclamos por daños superficiales o
         estéticos.
       </span>
-      <span className="w-1/2 text-justify mx-auto mb-8">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-8">
         5.3. La empresa no se responsabiliza por daños ocasionados durante el
         transporte.
       </span>
 
-      <h4 className="text-center w-full text-[5vw] font-medium md:text-[6vw] lg:text-lg mb-4">
+      <h4 className="text-center w-full text-[1rem] md:text-[1.2rem] font-medium   mb-4">
         6. ALMACENAJE Y RETIRO
       </h4>
-      <span className="w-1/2 text-justify mx-auto mb-4">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4">
         6.1. Si el cliente no retira el producto dentro de los 30 días
         calendario posteriores a la fecha estipulada de entrega, el taller
         enviará una comunicación formal solicitando el retiro del producto en un
         plazo máximo de 2 meses contados a partir del envío de dicha
         comunicación.
       </span>
-      <span className="w-1/2 text-justify mx-auto mb-8">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-8">
         6.2. Si transcurridos esos 2 meses el cliente no ha retirado el
         producto, se entenderá legalmente que ha abandonado el bien, conforme a
         lo establecido en el DECRETO 1413 DE 2018. En consecuencia, el taller
@@ -179,66 +179,66 @@ export default function Terms() {
         alguna.
       </span>
 
-      <h4 className="text-center w-full text-[5vw] font-medium md:text-[6vw] lg:text-lg mb-4">
+      <h4 className="text-center w-full text-[1rem] md:text-[1.2rem] font-medium   mb-4">
         7. CONTACTO Y COMUNICACIÓN
       </h4>
-      <span className="w-1/2 text-justify mx-auto mb-4">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4">
         7.1. Toda comunicación debe realizarse mediante los canales oficiales
         informados (WhatsApp, teléfono, correo electrónico). Cualquier acuerdo
         verbal o modificación debe ser confirmado por escrito.
       </span>
-      <span className="w-1/2 text-justify mx-auto mb-8">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-8">
         7.2. El cliente es responsable de proporcionar información de contacto
         correcta y mantener comunicación fluida durante el proceso de
         fabricación.
       </span>
 
-      <h4 className="text-center w-full text-[5vw] font-medium md:text-[6vw] lg:text-lg mb-4">
+      <h4 className="text-center w-full text-[1rem] md:text-[1.2rem] font-medium   mb-4">
         8. MODIFICACIONES A LOS TÉRMINOS Y CONDICIONES
       </h4>
-      <span className="w-1/2 text-justify mx-auto mb-8">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-8">
         8.1. La empresa podrá modificar los presentes términos y condiciones sin
         previo aviso. Las versiones actualizadas se aplicarán a los pedidos
         realizados posteriormente a la fecha de publicación de las
         modificaciones.
       </span>
 
-      <h4 className="text-center w-full text-[5vw] font-medium md:text-[6vw] lg:text-lg mb-4">
+      <h4 className="text-center w-full text-[1rem] md:text-[1.2rem] font-medium  mb-4">
         9. ACEPTACIÓN DE LOS TÉRMINOS
       </h4>
-      <span className="w-1/2 text-justify mx-auto mb-8">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-8">
         9.1. Al confirmar su pedido, el cliente declara haber leído, comprendido
         y aceptado todos los términos y condiciones descritos en este documento.
       </span>
 
-      <h4 className="text-center w-full text-[5vw] font-medium md:text-[6vw] lg:text-lg mb-4">
+      <h4 className="text-center w-full text-[1rem] md:text-[1.2rem] font-medium   mb-4">
         10. POLÍTICA DE CANCELACIÓN DE PEDIDOS
       </h4>
-      <span className="w-1/2 text-justify mx-auto mb-4">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4">
         10.1. Una vez abonado el anticipo, el pedido entra en proceso de
         fabricación, por lo que no se aceptan cancelaciones ni se realizan
         reembolsos del monto entregado.
       </span>
-      <span className="w-1/2 text-justify mx-auto mb-4">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-4">
         10.2. En caso de que el cliente desista del pedido una vez comenzada la
         producción, el taller se reserva el derecho de conservar el anticipo
         como compensación por los materiales adquiridos y la mano de obra ya
         invertida.
       </span>
-      <span className="w-1/2 text-justify mx-auto mb-20">
+      <span className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-20">
         10.3. Si el cliente no cumple con el pago total dentro del plazo
         estipulado o no retira el mueble dentro de los 30 días posteriores a la
         notificación de finalización, la empresa podrá disponer del producto sin
         obligación de reembolso.
       </span>
-      <p className="w-1/2 text-justify mx-auto mb-8">
+      <p className="w-full px-4 lg:p-0 lg:w-1/2 text-justify mx-auto mb-8">
         Agradecemos profundamente su confianza en nuestro trabajo.
         Cada mueble que fabricamos es único, hecho con dedicación y esmero para
         cumplir con sus expectativas. Si tiene dudas o necesita asistencia, no
         dude en comunicarse con nosotros a través de nuestros canales oficiales.
       </p>
 
-      {/* <h4 className="text-center w-full text-[5vw] font-medium md:text-[6vw] lg:text-lg mb-4"></h4>
+      {/* <h4 className="text-center w-full text-[1rem] md:text-[1.2rem] font-medium   mb-4"></h4>
       <span className="w-3/5 text-justify mx-auto mb-4"></span>
       <span className="w-3/5 text-justify mx-auto mb-4"></span>
       <span className="w-3/5 text-justify mx-auto mb-4"></span>
